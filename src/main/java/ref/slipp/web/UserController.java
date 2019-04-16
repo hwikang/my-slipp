f9e9c62d3c10bc4239e70153cc239d1375534180
haha <khdrogba@gmail.com>
2019-04-16 23:43:29 +0900

    04/16

--- src/main/java/ref/slipp/web/UserController.java
+++ src/main/java/ref/slipp/web/UserController.java
@@ -1,14 +1,27 @@
 package ref.slipp.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+	private List<User> users = new ArrayList<User>();
+	
 	@PostMapping("/create")
 	public String create(User user) {  //setter가 있으면 이렇게 한번에 user를 불러내서 세팅가능
 		System.out.println(user);  //toString 하면 걍 뽑네?
-		return "index";
+		users.add(user);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users",users);
+		return "list";
 	}
 }
